ed698a21bddffcb32455f12e4e706394c5dad559
kobisha89 <kobisha89@gmail.com>
2021-03-06 21:05:42 +0100

    Interceptor added

--- tasks-front/src/apis/Axios.js
+++ tasks-front/src/apis/Axios.js
@@ -4,4 +4,14 @@ var Axios = axios.create({
   baseURL: 'http://localhost:8080/api',
 });
 
+Axios.interceptors.request.use(
+  function success(config){
+    const token = window.localStorage['jwt'];
+    if (token){
+      config.headers['Authorization'] = 'Bearer ' + token;
+    }
+    return config;
+  }
+);
+
 export default Axios;
